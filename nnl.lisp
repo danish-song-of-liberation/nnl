72831dd79ea641ef530728b8051b882c4872565c
123456 qwerty <nefronbi@yandex.ru>
2025-06-16 18:57:14 +0300

    main system file

--- nnl.lisp
+++ nnl.lisp
@@ -1,12 +1,15 @@
 (asdf:defsystem :nnl
   :license "MIT"
   :version "0.1.0"
+  :homepage "https://github.com/danish-song-of-liberation/nnl"
+  :description "Common lisp neural networks mvp framework"
   :depends-on (:fiveam :magicl)
+  :serial t
   :components ((:module "src"
 				:components ((:module "math"
 							  :components ((:module "autodiff"
-											:components ((:file "autodiff" :type :lisp-source)
-														 (:file "autodiff-d" :type :lisp-source)
-														 (:file "autodiff-h" :type :lisp-source)))
-											(:file "numgrad" :type :lisp-source)))))))
+											:components ((:file "autodiff" :type "lisp")
+														 (:file "autodiff-d" :type "lisp")
+														 (:file "autodiff-h" :type "lisp")))
+											(:file "numgrad" :type "lisp")))))))
 											
