85339a40b0d2eda5207891dfb5f8a9f97c912b9f
123456 qwerty <nefronbi@yandex.ru>
2025-06-18 00:58:21 +0300

    fix

--- nnl.lisp
+++ nnl.lisp
@@ -1,3 +1,8 @@
+#| Philipp Mainlender (1841 - 1876)
+   -
+   Gott ist gestorben und sein 
+   tod war das leben der welt. |#
+
 (asdf:defsystem :nnl
   :license "MIT"
   :version "0.1.0"
@@ -11,8 +16,11 @@
 										   (:file "coerce-data" :type "lisp")	
 										   (:file "operations" :type "lisp")
 									       (:file "high-level" :type "lisp")))
+										   
 							 (:module "math"
-							  :components ((:module "autodiff"
+							  :components ((:file "math" :type "lisp")
+										   (:file "phi" :type "lisp")
+										   (:module "autodiff"
 											:components ((:file "autodiff" :type "lisp")
 														 (:file "autodiff-d" :type "lisp")
 														 (:file "autodiff-i" :type "lisp")
@@ -29,6 +37,8 @@
   "Global variable that determines the floating-point 
    number system used for calculations within a specific 
    mathematical functionality")
+   
+(defparameter *leakyrelu-default-shift* 0.01)
 						
 (setf *read-default-float-format* *calculus-system*)
 
