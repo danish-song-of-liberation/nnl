d329cb2fd361277c38e90d26aae8d7daf5d27aac
123456 qwerty <nefronbi@yandex.ru>
2025-06-27 02:40:37 +0300

    added average

--- src/utils/utils.lisp
+++ src/utils/utils.lisp
@@ -1,6 +1,6 @@
 (defpackage :nnl.utils
   (:use :cl)
-  (:export :clip-grad :binary-threashold))
+  (:export :clip-grad :binary-threashold :average))
   
 (in-package :nnl.utils)
 
@@ -8,4 +8,8 @@
   (if (> x 0.5)
     1
     0))	
-  
+	
+(defmethod average ((tensor nnl.math.autodiff:tensor) &aux (size 0))
+  (magicl:map #'(lambda (x) (incf size)) (nnl.math.autodiff::data tensor)) ;WTF
+  (/ (nnl.magicl:sum (nnl.math.autodiff::data tensor)) size))
+	
